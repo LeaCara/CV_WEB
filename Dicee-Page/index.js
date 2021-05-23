e5cd703cd00d18d191f25f1824da0942fa154180
lcarancini <leandro.carancini@gmail.com>
2021-05-23 20:34:25 -0300

    Adding solution + Improving use of attribute

--- Dicee-Page/index.js
+++ Dicee-Page/index.js
@@ -1,5 +1,6 @@
+var name1 = prompt("Player...");
 var dice1 = Math.trunc(Math.random() * 6) + 1;
 var dice2 = Math.trunc(Math.random() * 6) + 1;
-document.querySelector("div.Dicees img.dice1").src = dice1 + '.png';
-document.querySelector("div.Dicees img.dice2").src = dice2 + '.png';
-document.querySelector("div.Result span.pro").textContent = dice1 + dice2;
+document.querySelector("div.Dicees img.dice1").setAttribute("src", dice1 + '.png');
+document.querySelector("div.Dicees img.dice2").setAttribute("src", dice2 + '.png');
+document.querySelector("div.Result span.pro").textContent = dice1 + dice2 + " for " + name1;
