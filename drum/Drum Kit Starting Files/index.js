afa44b34f00d569a1969eb5e462337ccab13e464
LeaCara <leandro.carancini@gmail.com>
2021-05-26 15:48:08 -0300

    Finish Drum web

--- drum/Drum Kit Starting Files/index.js
+++ drum/Drum Kit Starting Files/index.js
@@ -1,7 +1,7 @@
 // Cantidad de letras
 var numberOfDrumButtons =  document.querySelectorAll(".drum").length;
 // Array de botones con letras, sonidos y variables
-var button = [];
+var buttonInstrum = [];
 // Listado de sonidos
 var sound = ['sounds\\tom-1.mp3', 'sounds\\tom-2.mp3', 'sounds\\tom-3.mp3', 'sounds\\tom-4.mp3', 'sounds\\crash.mp3', 'sounds\\kick-bass.mp3', 'sounds\\snare.mp3'];
 // Constructor de objetos button
@@ -15,39 +15,68 @@ function Button(letter, sound) {
 // Se cargan todos los botones del DOM
 for (var i = 0; i < numberOfDrumButtons ; i++){
   var but = new Button(document.querySelectorAll(".drum")[i].innerHTML,sound[i]);
-  button.push(but);
+  buttonInstrum.push(but);
 }
 
-// Crea evventos para todos los clicks de los buttons
+// Detectar clicks de los buttons
 for (var i = 0; i < numberOfDrumButtons ; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
     buttonLetter = this.innerHTML;
-    // MANERA COMPACTA, NECESITA ES6-> button.find(elem => elem.name === buttonLetter).sound();
-    switch(buttonLetter) {
-      case 'w':
-        button[0].sound();
-        break;
-      case 'a':
-        button[1].sound();
-        break;
-      case 's':
-        button[2].sound();
-        break;
-      case 'd':
-        button[3].sound();
-        break;
-      case 'j':
-        button[4].sound();
-        break;
-      case 'k':
-        button[5].sound();
-        break;
-      case 'l':
-        button[6].sound();
-        break;
-      default:
-        console.log(buttonLetter + "unknown");
-    }
+    makeSound(buttonLetter);
+    buttonAnimation(buttonLetter)
+
   });
 }
+
+// Detectar teclas presionadas
+document.addEventListener("keydown", function(event) {
+  // Regitrar tecla
+  keyboardLetter = event.key;
+  makeSound(keyboardLetter);
+  buttonAnimation(keyboardLetter);
+});
+
+function makeSound(buttonLetter) {
+  // MANERA COMPACTA, NECESITA ES6->
+  // Buscar instrumento según letra
+  var instrument = buttonInstrum.find(elem => elem.letter === buttonLetter);
+  // Si existe instrumernto
+  if (instrument)
+    instrument.sound();
+}
+
+function buttonAnimation(button){
+  var activeButton = document.querySelector("." + button);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+/***ANOTHER FORM TU SOLVE WITH SWITCH***/
+// switch(buttonLetter) {
+//   case 'w':
+//     button[0].sound();
+//     break;
+//   case 'a':
+//     button[1].sound();
+//     break;
+//   case 's':
+//     button[2].sound();
+//     break;
+//   case 'd':
+//     button[3].sound();
+//     break;
+//   case 'j':
+//     button[4].sound();
+//     break;
+//   case 'k':
+//     button[5].sound();
+//     break;
+//   case 'l':
+//     button[6].sound();
+//     break;
+//   default:
+//     console.log(buttonLetter + "unknown");
+// }
