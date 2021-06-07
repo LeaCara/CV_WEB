c976ee41a886d49d78d880e7504a0ff67909ac1b
lcarancini <leandro.carancini@gmail.com>
2021-06-06 22:44:19 -0300

    First commit jQuery

---
+++ JQuery/index.js
@@ -0,0 +1,6 @@
+// If i put script and jquery library in head section, it is required verify
+// if jquery is ready
+$(document).ready(function() {
+  alert("Hello");
+  $("h1").css("color", "red");
+});
